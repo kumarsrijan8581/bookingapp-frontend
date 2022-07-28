b1346df36e9aba2beefdeee96874ca3cbb637439
Kumar Srijan <kumarsrijan8581@gmail.com>
2022-07-28 11:13:55 +0530

    Ready

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const {user ,loading, error, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate()
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">lamabooking</span>
+        </Link>
+        {user ? <div className="navItems">
+            <span>{user.username}</span>
+            <button className="navButton" onClick={e=>{
+              dispatch({
+                type:"LOGOUT" 
+              })
+              navigate("/login");
+            }}>Logout</button>
+          </div> : (
+          <div className="navItems">
+            <button className="navButton" onClick={e=>{navigate("/register")}}>Register</button>
+            <button className="navButton" onClick={e=>{navigate("/login")}}>Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
